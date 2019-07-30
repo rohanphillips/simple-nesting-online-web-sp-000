68f076bb9af4e5668e0968c43d3083fc45fff7d5
Rohan Phillips <rohan@solutions-insurance.com>
2019-07-30 18:28:58 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,3 +1,4 @@
+require "pry"
 
 def hopper
 	programmer_hash =
@@ -83,6 +84,7 @@ def adding_matz
      }
 
 	programmer_hash[:yukihiro_matsumoto] = {}
+	binding.pry
 	programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
 	programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
 end
